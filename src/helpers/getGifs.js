ebc5c83568343723ff9e6bf071f00218eac66f4f
Stivens Aliaga <stivensaliaga@gmail.com>
2025-01-30 18:18:57 -0500

    gif expert app completed

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async( category ) => {
 
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=kaJ1JwD4CuQgYun7YpTQpTr5p1qs1sQn&q=${ category }&limit=10`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=iwriIlRQtb2LddqWC28gk47hAf0AJLS3&q=${ category }&limit=10`;
     const resp = await fetch( url );
     const { data } = await resp.json();
 
